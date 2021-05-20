6696d38ed62b3d3c1d8d252dc350774b3e1fb684
Ibra <ibragimgiyazov@gmail.com>
2021-05-21 00:51:00 +0300

    Add crud

--- mysite/urls.py
+++ mysite/urls.py
@@ -2,13 +2,19 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
-from song.views import homepage, about
+from song.views import homepage, song_create, song_delete, song_detail, song_edit
+from account.views import signin, signout, signup
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', homepage),
-    path('about/', about)
+    path('<int:id>', song_detail),
+    path('edit/<int:id>', song_edit),
+    path('delete/<int:id>', song_delete),
+    path('create', song_create),
+    path('account/signin', signin),
+    path('account/signup', signup),
 ]
 
 if settings.DEBUG:
--- song/models.py
+++ song/models.py
@@ -4,12 +4,12 @@ import datetime
 class Song(models.Model):
     title = models.CharField("Название песни", max_length=50)
     age = models.DateField("Дата выпуска", default = datetime.timedelta(0))
-    image = models.ImageField("Изображение", upload_to = "songs_posters/")
+    image = models.ImageField("Изображение", upload_to = "songs_posters/", null=True, blank=True)
     artist = models.CharField("Исполнитель", max_length=50)
     album = models.CharField("Альбом", max_length=70)
     ganre = models.CharField("Жанр", max_length=70)
-    audio = models.FileField("Песня",upload_to = "songs_audio/", null = True)
-    url = models.SlugField(max_length=160, unique = True)
+    audio = models.FileField("Песня",upload_to = "songs_audio/", null = True, blank=True)
+    url = models.SlugField(max_length=160, unique = True, null=True, blank=True )
 
     def __str__(self):
         return self.title
